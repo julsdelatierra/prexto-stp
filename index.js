cde038387e2ca3a7638af576c7ad129f044722a8
Jul Ceballos <jul@startegyvb.com>
2019-02-27 09:23:34 -0600

    El metodo debe ser POST.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server=Hapi.server({
 
 // Add the route
 server.route({
-    method:'GET',
+    method:'POST',
     path:'/stp/state/change',
     handler: function(request, h) {
         const params = request.payload
