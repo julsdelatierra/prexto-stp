af572cdf5c4fa8feacaad0bf96b3cb0fffc01844
Jul Ceballos <jul@startegyvb.com>
2019-03-14 19:25:24 -0600

    Se agregó al servicio de STP el fake de uber.

--- index.js
+++ index.js
@@ -19,11 +19,29 @@ server.route({
             ok: true,
             data: {
                 id: '58fd1259',
-                estado: params.estado, //Devolución
+                estado: params.estado, // Éxito o Devolución
                 detalle: ''
             }
         };
     }
+});
+
+server.route({
+  method:'GET',
+  path:'/partners/me',
+  handler: (request, h) => {
+    return {
+      'driver_id': '8LvWuRAq2511gmr8EMkovekFNa2848lyMaQevIto-aXmnK9oKNRtfTxYLgPq9OSt8EzAu5pDB7XiaQIrcp-zXgOA5EyK4h00U6D1o7aZpXIQah--U77Eh7LEBiksj2rahB==',
+      'first_name': 'Joaquín',
+      'last_name': 'Moreno Pérez',
+      'email': 'uber.developer+tester@example.com',
+      'phone_number': '+14155550000',
+      'picture': 'https://d1w2poirtb3as9.cloudfront.net/16ce502f4767f17b120e.png',
+      'promo_code': 'ubert4544ue',
+      'rating': 5,
+      'activation_status': 'active'
+    };
+  }
 });
 
 // Start the server
